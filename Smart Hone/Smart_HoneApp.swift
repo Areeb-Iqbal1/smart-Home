87dce003ea2bc7e129b827c479806de55157a57f
Areeb H Iqbal <Iqbal.A@etoncollege.org.uk>
2024-11-28 12:49:48 +0000

    Initial Commit

---
+++ Smart Hone/Smart_HoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_HoneApp.swift
+//  Smart Hone
+//
+//  Created by Iqbal, Areeb (AMM) on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Smart_HoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Smart HoneTests/Smart_HoneTests.swift
@@ -0,0 +1,16 @@
+//
+//  Smart_HoneTests.swift
+//  Smart HoneTests
+//
+//  Created by Iqbal, Areeb (AMM) on 28/11/2024.
+//
+
+import Testing
+
+struct Smart_HoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
